da3421080bac4089a055c4b157dc0a4116588083
rhrudska987 <rhrudska987@naver.com>
2022-08-11 16:41:37 +0900

    ubuntu에서 git으로

---
+++ dir1/pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void swap(int *x, int *y){
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main(void){
+	int x = 1;
+	int y = 2;
+	swap(&x, &y);
+	printf("%d %d", x, y);
+	return 0;
+}
